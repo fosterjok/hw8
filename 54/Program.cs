d773fb14efe9763c60e9368b2f7a461e818df968
fosterjok <fosterjok@mail.ru>
2022-10-25 23:06:12 +0300

    54

---
+++ 54/Program.cs
@@ -0,0 +1,48 @@
+﻿/* Задайте двумерный массив. 
+Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.*/
+
+
+int[,] array = new int[4, 4];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(10);
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+
+}
+
+for (int i = 0; i < array.GetLength(0); i++)
+
+{
+    for (int k = 0; k < array.GetLength(1) - 1; k++)
+    {
+
+        for (int j = 0; j < array.GetLength(1) - 1; j++)
+        {
+
+            if (array[i, j] < array[i, j + 1])
+            {
+                int temp = array[i, j];
+                array[i, j] = array[i, j + 1];
+                array[i, j + 1] = temp;
+            }
+
+        }
+    }
+}
+Console.WriteLine();
+for (int i = 0; i < array.GetLength(0); i++)
+
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+
+}
